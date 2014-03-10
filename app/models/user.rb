0bcbea45fdac688bf22599ab4ccc2de1af710104
gustavomv066 <fenriz006@gmail.com>
2014-03-10 18:55:45 -0300

    role

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  def get_role
-    self.role
-  end
+
 end
