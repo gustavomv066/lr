16c3cf7ef18f52764e4e6e64afb5b553500107fd
gustavomv066 <fenriz006@gmail.com>
2014-03-11 12:05:35 -0300

    set role method on model user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,13 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  def set_role
+    if self.role == 1
+      self.role = 2
+    else
+      self.role = 1
+    end
+  end
+
 
 end
