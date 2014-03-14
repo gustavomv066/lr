2098bdce171c7fc2dce83f3aca2b6035290155ac
gustavomv066 <fenriz006@gmail.com>
2014-03-14 14:24:46 -0300

    identation and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,11 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 User.create(email: 'email0@email.com' , password: 'password', password_confirmation: 'password' , role: 2 ) ##admin
+
+#users
+
+User.create(email: 'email1@email.com' , password: 'password', password_confirmation: 'password' , role: 1 )
+User.create(email: 'email2@email.com' , password: 'password', password_confirmation: 'password' , role: 1 )
+User.create(email: 'email3@email.com' , password: 'password', password_confirmation: 'password' , role: 1 )
+User.create(email: 'email4@email.com' , password: 'password', password_confirmation: 'password' , role: 1 )
+
