c86dfe79cbe74bddde3e7d122c92d1b2f662d67e
gustavomv066 <fenriz006@gmail.com>
2014-03-14 13:09:11 -0300

    controller guitar action create

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :comments
+  has_many :guitars
 
 
   def set_role
